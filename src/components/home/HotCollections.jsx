1dba09455d5d1d3a55070daa927153540b1d40b7
Jake <jakemoccia@gmail.com>
2024-11-25 17:11:11 -0500

    Fixing New Items Section

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
